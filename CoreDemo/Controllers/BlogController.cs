75867bc15f8ccf8282b8dae4e43f93d34d00bbb9
Merve Kardes <mmervekardess@gmail.com>
2021-10-15 13:56:37 +0300

    .

--- CoreDemo/Controllers/BlogController.cs
+++ CoreDemo/Controllers/BlogController.cs
@@ -8,13 +8,17 @@ using System.Threading.Tasks;
 
 namespace CoreDemo.Controllers
 {
-    public class CategoryController : Controller
+    public class BlogController : Controller
     {
-        CategoryManager cm = new CategoryManager(new EfCategoryRepository());
+        BlogManager bm = new BlogManager(new EfBlogRepository());
         public IActionResult Index()
         {
-            var values = cm.GetList();
+            var values = bm.GetBlogListWithCategory();
             return View(values);
+        }
+        public IActionResult BlogDetails(int id)
+        {
+            return View();
         }
     }
 }
